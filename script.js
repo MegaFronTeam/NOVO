898c50b451cd987e729a9dedcef289cc53818e56
PAVEL KHALUNIN <wol1414@gmail.com>
2024-08-18 20:58:29 +0200

    fix: add  transition scroll in fixed element

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+let baseScrollY = 0;
 function checkScreenWidth() {
   if (window.innerWidth < 1000) {
-    // Code to execute if screen width is less than 1000 pixels
-    console.log('Screen width is less than 1000 pixels');
+    document.querySelector('.height-mobile--js').style.minHeight =
+      document.querySelector('.secondTextColor').offsetHeight +
+      document.querySelector('.cards--js').offsetHeight +
+      'px';
+    if (document.querySelector('.cards--js').classList.contains('showFullItemsList')) {
+      if (baseScrollY === 0) {
+        baseScrollY = window.scrollY;
+      }
+      let currentScrollY = window.scrollY;
+      let scrollDifference = currentScrollY - baseScrollY;
+
+      console.log('scrollDifference', scrollDifference);
+      console.log('currentScrollY', currentScrollY);
+      console.log('baseScrollY', baseScrollY);
+
+      let height = document.querySelector('.cards--js').offsetHeight;
+      let windowHeight = window.innerHeight;
+      if (height > windowHeight) {
+      }
+      document.querySelector('.cards--js').style.transform = `translateY(-${scrollDifference}px)`;
+      document.querySelector(
+        '.secondTextColor',
+      ).style.transform = `translateY(-${scrollDifference}px)`;
+      // console.log('document', document.querySelector('.cards--js').offsetTop);
+      // console.log('scrollY', window.scrollY);
+    }
   } else {
     // Code to execute if screen width is greater than or equal to 1000 pixels
     console.log('Screen width is 1000 pixels or greater');
   }
 }
 
-checkScreenWidth();
 document.addEventListener('scroll', checkScreenWidth, false);
+document.addEventListener('DOMContentLoaded', checkScreenWidth, false);
